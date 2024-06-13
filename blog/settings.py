c798708c038cf4eb9a73cc5ce452c945e585e4bd
e_m <kfmty135shk@gmail.com>
2024-06-13 14:05:06 +0300

    edit csrf trusted origins

--- blog/settings.py
+++ blog/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ["https://moonsands-edinstvo-5488.twc1.net/"]
+CSRF_TRUSTED_ORIGINS = ["https://moonsands-edinstvo-5488.twc1.net"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
