016316be264c570f310ed52d7038d03195622a2e
e_m <kfmty135shk@gmail.com>
2024-06-25 13:13:52 +0300

    Fix permission bug

--- article_feed/views.py
+++ article_feed/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from .serializers import *
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
